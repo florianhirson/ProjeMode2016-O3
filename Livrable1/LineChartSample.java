3b4933eea4872eab4717630b1587014f38537c9b
Florian BARBET <florian.barbet@etudiant.univ-lille1.fr>
2017-03-02 18:18:31 +0100

    Ajout du .java exemple pour création d'une courbe avec un csv basé sur un exemple de LineChart

---
+++ Livrable1/LineChartSample.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+ 
+ 
+public class LineChartSample extends Application {
+ 
+     @SuppressWarnings({ "unchecked", "rawtypes" })
+	public void start(Stage stage) throws IOException {
+        stage.setTitle("Line Chart Sample");
+        //defining the axes
+        final NumberAxis xAxis = new NumberAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("Number of Month");
+        //creating the chart
+        final LineChart<Number,Number> lineChart = 
+                new LineChart<Number,Number>(xAxis,yAxis);
+                
+        lineChart.setTitle("Stock Monitoring, 2010");
+        //defining a series
+        XYChart.Series series = new XYChart.Series();
+        series.setName("My portfolio");
+        //populating the series with data
+        
+        
+         
+        try
+        {
+           String chemin = "C:/Users/florian/Desktop/Recherche/Java/ProjetS3/Test.csv";
+           BufferedReader fichier_source = new BufferedReader(new FileReader(chemin));
+           String chaine;
+          
+           ArrayList<String[]> tabChaine = new ArrayList<String[]>();
+           
+           int indice = 0;
+           while((chaine = fichier_source.readLine())!= null)
+           {
+            tabChaine.add(chaine.split(","));
+            indice++;
+           }
+           fichier_source.close();
+           for(int i = 2; i < indice-1 ; i++)
+        	   for(int j = 0; j < tabChaine.get(i).length ; j++ )
+             	series.getData().add(new XYChart.Data(Integer.parseInt(tabChaine.get(i)[j]), Integer.parseInt(tabChaine.get(i)[j])));
+        }
+        catch (FileNotFoundException e)
+        {
+           System.out.println("Le fichier est introuvable !");
+        }
+        
+        
+        
+        Scene scene  = new Scene(lineChart,800,600);
+        lineChart.getData().add(series);
+       
+        stage.setScene(scene);
+        stage.show();
+    }
+ 
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
