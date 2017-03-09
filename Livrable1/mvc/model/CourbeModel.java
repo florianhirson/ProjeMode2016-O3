72f9e951d2eb23fc2f1c68cf9c2442de805f2cfd
Florian BARBET <florian.barbet@etudiant.univ-lille1.fr>
2017-03-09 17:21:36 +0100

    Update CourbeModel.java

--- Livrable1/mvc/model/CourbeModel.java
+++ Livrable1/mvc/model/CourbeModel.java
@@ -3,7 +3,8 @@ package mvc.model;
 import java.util.Observable;
 /**
  * Model du MVC sur la Courbe
- * @author florian
+ * @author Florian Barbet
+ * @author Thomas Mastalerz
  *
  * @param <X>
  * @param <Y>
@@ -35,11 +36,78 @@ public class CourbeModel<X,Y> extends Observable {
 	}
 
 
+	public int sizeOfCourbe(){
+		return courbeData.sizeOfData();
+	}
+
+	public X getDataX(int i){
+		return courbeData.getX(i);
+	}
+
+	public Y getDataY(int i){
+		return courbeData.getY(i);
+	}
 	/**
 	 * 
 	 * TODO Methode de transformation de la courbe
 	 * 
 	 **/
 
+	/**
+	 * TransfoLog transformation sur la courbe avec la fonction log
+	 * @author Thomas
+	 * @param c
+	 */
+	public void transfoLog4Double(Courbe<Double,Double> c){
+		int taille = c.sizeOfData();
+
+		double dataX;
+		double dataY;
+
+		for(int i=0; i<taille; i++){
+			dataX = Math.log((double)c.getX(i));
+			dataY = Math.log((double)c.getY(i));
+
+			c.addXY(dataX,dataY);
+		}
+
+
+	}
+
+	/**
+	 * @see CourbeModel#transfoLog4Double(Courbe)
+	 * @param c
+	 */
+	public void transfoLog4Integer(Courbe<Integer,Integer> c){
+		int taille = c.sizeOfData();
+
+		int dataX;
+		int dataY;
+
+		for(int i=0; i<taille; i++){
+			dataX = (int) Math.log((double)c.getX(i));
+			dataY = (int) Math.log((double)c.getY(i));
+
+			c.addXY(dataX,dataY);
+		}
+	}
+
+	/**
+	 * @see CourbeModel#transfoLog4Double(Courbe)
+	 * @param c
+	 */
+	public void transfoLog4String(Courbe<String,String> c){
+		int taille = c.sizeOfData();
+
+		double dataX;
+		double dataY;
+
+		for(int i=0; i<taille; i++){
+			dataX=Math.log(Double.parseDouble(c.getX(i)));
+			dataY=Math.log(Double.parseDouble(c.getY(i)));
+
+			c.addXY(dataX+"",dataY+"");
+		}
+	}
 
 }
