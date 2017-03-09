2e5af10ab1ed45d7d22c2c99a5cff26db5454a06
Florian BARBET <florian.barbet@etudiant.univ-lille1.fr>
2017-03-09 16:51:01 +0100

    Ajout de la vue

---
+++ Livrable1/mvc/view/CourbeVue.java
@@ -0,0 +1,55 @@
+package mvc.view;
+
+import java.util.Observer;
+
+import javafx.scene.Scene;
+import javafx.scene.chart.Axis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+import mvc.control.CourbeController;
+import mvc.model.CourbeModel;
+
+public abstract class CourbeVue<X,Y> extends Stage implements Observer {
+	protected CourbeModel<X,Y> model ;
+	protected CourbeController<X,Y> controller ;
+	final Axis<X> xAxis;
+	final Axis<Y> yAxis;
+	final LineChart<X,Y> lineChart;
+	@SuppressWarnings("rawtypes")
+	XYChart.Series series = new XYChart.Series();
+
+
+	@SuppressWarnings({"unchecked", "rawtypes" })
+	public CourbeVue(CourbeModel<X,Y> mod, CourbeController<X,Y> cont,Axis<X> xAx,Axis<Y> yAx,String t){
+		super();
+		super.setTitle("Projet Modelisation");
+		model = mod;
+		controller = cont;
+		xAxis=xAx;
+		yAxis=yAx;
+		xAxis.setLabel("Abcisse");
+		yAxis.setLabel("Ordonnee");
+		lineChart = new LineChart<X,Y>(xAxis,yAxis);
+		lineChart.setTitle("Fonction f(x)= "+t);
+		//definition de la serie
+		series.setName(t);
+		// remplir la serie de donnees
+		for(int i = 0; i < model.sizeOfCourbe()  ; i++)
+		{
+			series.getData().add(new XYChart.Data(model.getDataX(i), model.getDataY(i)));
+		}
+
+		Scene scene  = new Scene(lineChart,800,600);
+		lineChart.getData().add(series);
+
+		this.setScene(scene);
+
+	}
+
+
+	protected CourbeModel<X,Y> model (){
+		return model ;
+	}
+
+}
