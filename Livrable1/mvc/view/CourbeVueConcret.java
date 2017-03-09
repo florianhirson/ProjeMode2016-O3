dfefc969958e7ef42dfb9e0bccd98df51064da18
Florian BARBET <florian.barbet@etudiant.univ-lille1.fr>
2017-03-09 16:51:34 +0100

    Ajout de la classe concrete

---
+++ Livrable1/mvc/view/CourbeVueConcret.java
@@ -0,0 +1,24 @@
+package mvc.view;
+
+import java.util.Observable;
+
+import javafx.scene.chart.Axis;
+import mvc.control.CourbeController;
+import mvc.model.CourbeModel;
+
+public class CourbeVueConcret<X, Y> extends CourbeVue<X,Y> {
+
+	
+
+	public CourbeVueConcret(CourbeModel<X, Y> mod, CourbeController<X, Y> cont, Axis<X> xAx, Axis<Y> yAx, String t) {
+		super(mod, cont, xAx, yAx, t);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
