1a6a3a173fc632c53084da30f109c46d09a3a905
Florian BARBET <florian.barbet@etudiant.univ-lille1.fr>
2017-03-09 15:55:45 +0100

    Ajout du modele

---
+++ Livrable1/mvc/model/CourbeModel.java
@@ -0,0 +1,45 @@
+package mvc.model;
+
+import java.util.Observable;
+/**
+ * Model du MVC sur la Courbe
+ * @author florian
+ *
+ * @param <X>
+ * @param <Y>
+ */
+public class CourbeModel<X,Y> extends Observable {
+
+	/**
+	 * Courbe<X,Y> courbe sur laquelle toute transformation passera
+	 */
+	private Courbe<X,Y> courbeData = new Courbe<X,Y>();
+
+	/**
+	 * Renvoie la courbe accession
+	 * @return courbeData
+	 */
+	public Courbe<X,Y> getCourbe(){
+		return courbeData;
+	}
+
+
+	/**
+	 * Permet de modifier la courbe et initialiser
+	 * @param c
+	 */
+	public void setCourbe(Courbe<X,Y> c){
+		courbeData = c;
+		setChanged();
+		notifyObservers();
+	}
+
+
+	/**
+	 * 
+	 * TODO Methode de transformation de la courbe
+	 * 
+	 **/
+
+
+}
