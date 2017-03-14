ff7d93a75ee1cb1b88bb4a031e53112442a61e8a
Florian <flohirson@gmail.com>
2017-03-14 15:03:36 +0100

    Modification du SelectfileChooser pour respecter le MVC (WIP)

--- ProjeModeO3/src/Test/SelectFileChooserView.java
+++ ProjeModeO3/src/Test/SelectFileChooserView.java
@@ -1,6 +1,5 @@
 package Test;
 
-import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
@@ -15,37 +14,38 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.geometry.Pos;
 import javafx.geometry.Insets;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import java.io.File;
 
-public class SelectFileChooserExample extends Application {
+public class SelectFileChooserView extends Stage{
 
 	private Text actionStatus;
 	private static final String titleTxt = "JavaFX File Chooser Example 1";
+	Label label;
+	HBox labelHb;
+	Button btn1;
+	HBox buttonHb1;
+	VBox vbox;
+	Scene scene;
+	public Stage primaryStage;
+	String chemin;
 
-	public static void main(String [] args) {
-
-		Application.launch(args);
-	}
-
-	@Override
-	public void start(Stage primaryStage) {
-	
+	SelectFileChooserView() {
+		super();
+		primaryStage = new Stage();
 		primaryStage.setTitle(titleTxt);	
 
 		// Window label
-		Label label = new Label("Select File Choosers");
+		label = new Label("Select File Choosers");
 		label.setTextFill(Color.DARKBLUE);
 		label.setFont(Font.font("Calibri", FontWeight.BOLD, 36));
-		HBox labelHb = new HBox();
+		labelHb = new HBox();
 		labelHb.setAlignment(Pos.CENTER);
 		labelHb.getChildren().add(label);
 
 		// Buttons
-		Button btn1 = new Button("Choisissez un fichier...");
-		btn1.setOnAction(new SingleFcButtonListener());
-		HBox buttonHb1 = new HBox(10);
+		btn1 = new Button("Choisissez un fichier...");
+		btn1.setOnAction(e-> showSingleFileChooser());
+		buttonHb1 = new HBox(10);
 		buttonHb1.setAlignment(Pos.CENTER);
 		buttonHb1.getChildren().addAll(btn1);
 
@@ -56,42 +56,35 @@ public class SelectFileChooserExample extends Application {
 		actionStatus.setFill(Color.FIREBRICK);
 
 		// Vbox
-		VBox vbox = new VBox(30);
+		vbox = new VBox(30);
 		vbox.setPadding(new Insets(25, 25, 25, 25));;
 		vbox.getChildren().addAll(labelHb, buttonHb1, actionStatus);
 
 		// Scene
-		Scene scene = new Scene(vbox, 500, 300); // w x h
+		scene = new Scene(vbox, 500, 300); // w x h
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
 
 	}
 
-	private class SingleFcButtonListener implements EventHandler<ActionEvent> {
 
-		@Override
-		public void handle(ActionEvent e) {
-
-			showSingleFileChooser();
-		}
-	}
 
 	private void showSingleFileChooser() {
-	
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.getExtensionFilters().addAll(new ExtensionFilter("Fichiers CSV", "*.csv"));
 		File selectedFile = fileChooser.showOpenDialog(null);
-		
 
 		if (selectedFile != null) {
-
 			actionStatus.setText("Fichier choisis: " + selectedFile.getName());
-			String chemin = selectedFile.getAbsolutePath();
-			System.out.println(chemin);
+			chemin = selectedFile.getAbsolutePath();
 		}
 		else {
 			actionStatus.setText("Selection de fichier annulee.");
 		}
+		
 	}
+
+
+
 }
