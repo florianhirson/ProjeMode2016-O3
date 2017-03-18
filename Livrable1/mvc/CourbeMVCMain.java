6111e27b967f21f60f2ee2a8f9e8d003372e290b
Florian BARBET <florian.barbet@etudiant.univ-lille1.fr>
2017-03-18 22:12:08 +0100

    Update CourbeMVCMain.java

--- Livrable1/mvc/CourbeMVCMain.java
+++ Livrable1/mvc/CourbeMVCMain.java
@@ -29,6 +29,8 @@ public class CourbeMVCMain extends Application{
 		
 		Courbe<Number,Number> log = new Courbe<Number,Number>();
 		Courbe<Number,Number> cmm = new Courbe<Number,Number>();
+		Courbe<Number,Number> csr = new Courbe<Number,Number>();
+		Courbe<Number,Number> cs = new Courbe<Number,Number>();
 		Courbe<Number,Number> cmd = new Courbe<Number,Number>();
 		System.out.println("Inserez le fichier de donnee a utiliser : ");
 		String data = sc.nextLine();
@@ -82,30 +84,85 @@ public class CourbeMVCMain extends Application{
 		CourbeVue<Number,Number> vue = new CourbeVueConcret<Number,Number>(model,control,new NumberAxis(),new NumberAxis(),data);
 		control.addView(vue);
 		
-		model.transfoLog4Num(log);
-		model.moyenneMobile(cmm);
-		model.desaisonaliser(cmd);
-		model.logistique(logis);
+		
+		
+		
+		
 	
 		CourbeModel<Number,Number> modelFusion = new CourbeModel<Number,Number>();
 		modelFusion.setCourbe(c);
 		CourbeController<Number,Number> controlF = new CourbeController<Number,Number>(modelFusion);
-		CourbeVue<Number,Number> vueF = new CourbeVueConcret<Number,Number>(modelFusion,controlF,new NumberAxis(),new NumberAxis(),"Test Fusion : "+data);
+		CourbeVue<Number,Number> vueF = new CourbeVueConcret<Number,Number>(modelFusion,controlF,new NumberAxis(),new NumberAxis(),"Fusion : "+data);
 		
 		controlF.addView(vueF);
-		vueF.addSeries(cmd, "St");
-		vueF.addSeries(cmm, "Mt");
-		vueF.addSeries(log, "logt");
-		vueF.addSeries(logis, "logis");
+		
+		int couAff = 0;
+		
+		
+		
+		couAff = 0;
+		System.out.println("Voir resultat Mt : Yes = 1 No = 0");
+		couAff=sc.nextInt();
+		if(Integer.valueOf(couAff)==1){
+			model.moyenneMobile(cmm,couAff);
+			vueF.addSeries(cmm, "Mt");
+		}
+		
+		couAff = 0;
+		System.out.println("Voir resultat Xt-Mt : Yes = 1 No = 0");
+		couAff=sc.nextInt();
+		if(Integer.valueOf(couAff)==1){
+			model.saisonResidu(csr,couAff);
+			vueF.addSeries(csr, "Xt-Mt");
+		}
+		
+		
+		couAff = 0;
+		System.out.println("Voir resultat St : Yes = 1 No = 0");
+		couAff=sc.nextInt();
+		if(Integer.valueOf(couAff)==1){
+			model.saison(cs,couAff);
+			vueF.addSeries(cs, "St");
+		}
+		
+		couAff = 0;
+		System.out.println("Voir resultat Xt-St : Yes = 1 No = 0");
+		couAff=sc.nextInt();
+		if(Integer.valueOf(couAff)==1){
+			model.desaisonaliser(cmd,couAff);
+			vueF.addSeries(cmd, "Xt-St");
+		}
+		
+		couAff = 0;
+		System.out.println("Voir resultat Log(Xt) >> Yt1 : Yes = 1 No = 0");
+		couAff=sc.nextInt();
+		if(Integer.valueOf(couAff)==1){
+			model.transfoLog4Num(log,couAff);
+			vueF.addSeries(log, "Yt1");
+		}
+		
+		
+		couAff = 0;
+		System.out.println("Voir resultat Log(Xt/(Xt-1)) >> Yt2 : Yes = 1 No = 0");
+		couAff=sc.nextInt();
+		if(Integer.valueOf(couAff)==1){
+			model.logistique(logis,couAff);
+			vueF.addSeries(logis, "Yt2");
+		}
+		
+		
+		
+		
 		
 		int choix = 0;
-		System.out.println("Afficher  ? Yes = 1 Non = 0");
+		System.out.println("Afficher  ? Yes = 1 No = 0");
 		choix = sc.nextInt();
 		if(Integer.valueOf(choix)==1)
 		{
 			vue.show();
 			vueF.show();
 		}
+		
 		
 		
 			
