1c6fc87fe9400454c59ce555a976f974b79e9734
Florian BARBET <florian.barbet@etudiant.univ-lille1.fr>
2017-03-24 19:12:55 +0100

    Update CourbeVueConcret.java

--- Livrable1/mvc/view/CourbeVueConcret.java
+++ Livrable1/mvc/view/CourbeVueConcret.java
@@ -3,6 +3,7 @@ package mvc.view;
 import java.util.Observable;
 
 import javafx.scene.chart.Axis;
+import javafx.scene.control.Tab;
 import mvc.control.CourbeController;
 import mvc.model.CourbeModel;
 
@@ -10,15 +11,18 @@ public class CourbeVueConcret<X, Y> extends CourbeVue<X,Y> {
 
 	
 
+	@SuppressWarnings("unchecked")
 	public CourbeVueConcret(CourbeModel<X, Y> mod, CourbeController<X, Y> cont, Axis<X> xAx, Axis<Y> yAx, String t) {
 		super(mod, cont, xAx, yAx, t);
-		// TODO Auto-generated constructor stub
-		model.addObserver(this);
+		lineChart.getData().add(series);
+
 	}
+	
+
 
 	@Override
 	public void update(Observable o, Object arg) {
-		// TODO Auto-generated method stub
+		
 		setDisplay(model().getCourbe());
 	}
 
