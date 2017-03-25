103829eb267b0fc390b993d0de3b51983c07100d
Florian BARBET <florian.barbet@etudiant.univ-lille1.fr>
2017-03-25 21:15:17 +0100

    Update CourbeTest.java

--- Livrable1/mvc/model/test/CourbeTest.java
+++ Livrable1/mvc/model/test/CourbeTest.java
@@ -2,31 +2,39 @@ package mvc.model.test;
 
 /*
  * @author Rayan Haddad
+ * @author Florian BARBET
  * */
 
 import static org.junit.Assert.assertEquals;
-
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.ArrayList;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import mvc.model.Courbe;
-import mvc.model.CourbeModel;
 
-// TEST POUR COURBE MODEL
+
+
 
 public class CourbeTest {
 	
 	Courbe<Number,Number> courbeData = new Courbe<Number,Number>();
-	CourbeModel<Number,Number> model = new CourbeModel<Number,Number>();
-	/*CourbeModel<Number,Number> lambdaPaire = new CourbeModel<Number,Number>();
-	CourbeModel<Number,Number> lambdaImpair = new CourbeModel<Number,Number>();*/
+	int taille=0;
 
+	@Before
+	public void setUp(){
+		for(int i = 1; i < 11;i++){
+			courbeData.addXY(i,i*2);
+			taille++;
+		}
+	}
+	
+	
+	@Test
+	public void sizeOfDataTest(){
+		assertEquals(taille,courbeData.sizeOfData());
+	}
+	
 	@Test
 	public void getXtest() {
 		Courbe<Integer,Integer> c = new Courbe<Integer,Integer>();
@@ -40,11 +48,40 @@ public class CourbeTest {
 		Courbe<Integer,Integer> c = new Courbe<Integer,Integer>();
 		c.addXY(2,5);
 		int y = c.getY(0);
-		assertEquals(2,y);
+		assertEquals(5,y);
+	}
+	
+	@Test
+	public void removeXYTest(){
+		int sizeDebut = Integer.valueOf(courbeData.sizeOfData());
+		courbeData.removeXY(0);
+		assertEquals(sizeDebut,courbeData.sizeOfData()+1); 
 	}
 
+	@Test
+	public void addXYTest(){
+		int sizeDebut = Integer.valueOf(courbeData.sizeOfData());
+		courbeData.addXY(40,20);
+		assertTrue(sizeDebut==courbeData.sizeOfData()-1);
+	}
+	
+	@Test
+	public void setYTest(){ 
+			
+		assertEquals(2,courbeData.getY(0));
+		courbeData.setY(0,20);
+		assertEquals(20,courbeData.getY(0));
+		
+	}
 
-
+	@Test
+	public void indexOfXYTest(){
+		int indexY = courbeData.indexOfXYbyY(4);
+		int indexX = courbeData.indexOfXYbyX(2);
+		
+		assertEquals(1,indexY);
+		assertEquals(1,indexX);
+	}
 
 
 
