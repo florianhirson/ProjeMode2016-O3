676bf3da4a14fca2791ab971cc108a1025bc6560
Florian BARBET <florian.barbet@etudiant.univ-lille1.fr>
2017-04-09 20:56:11 +0200

    Update CourbeModelPrevisionTest.java

--- livrable2/mvc/model/test/CourbeModelPrevisionTest.java
+++ livrable2/mvc/model/test/CourbeModelPrevisionTest.java
@@ -31,14 +31,35 @@ public class CourbeModelPrevisionTest {
 	}
 
 	@Test
+	public void keepdataSimpleTest(){
+		assertEquals(3.0,(double)c1.getY(0),0.1);
+		assertEquals(4.0,(double)c1.getY(1),0.1);
+		assertEquals(4.5,(double)c1.getY(2),0.1);	
+		assertEquals(4.0,(double)c1.getY(3),0.1);	
+	}
+	
+	@Test
+	public void keepdataDoubleTest(){
+		assertEquals(3.0,(double)c2.getY(0),0.1);
+		assertEquals(4.0,(double)c2.getY(1),0.1);
+		assertEquals(4.5,(double)c2.getY(2),0.1);	
+		assertEquals(4.0,(double)c2.getY(3),0.1);	
+	}
+	
+	@Test
 	public void lissageExpSimpleTest() {
-		assertEquals(3.8125,(double)c1.getY(3),0.0001);	
+		assertEquals(3.8125,(double)c1.getY(6),0.0001);	
+		assertEquals(3.8125,(double)c1.getY(5),0.0001);	
+		assertEquals(3.8125,(double)c1.getY(4),0.0001);	
+		
 	}
 
 	@Test
 	public void lissageExpDoubleTest(){
-		assertEquals(3.375,(double)c2.getY(4),0.001);	
+		assertEquals(3.375,(double)c2.getY(6),0.001);
 		assertEquals(3.375,(double)c2.getY(5),0.001);	
+		assertEquals(3.375,(double)c2.getY(4),0.001);	
+		
 	}
 	
 }
